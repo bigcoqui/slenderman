408de9d719248c8947cdb9306caf22633fbc4484
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-07-09 14:54:03 -0300

    ok

--- source/Main.hx
+++ source/Main.hx
@@ -33,7 +33,7 @@ class Main extends Sprite
 	{
 		super();
 
-		SUtil.gameCrashCheck();
+		SUtil.uncaughtErrorHandler();
 
 		if (stage != null)
 		{
@@ -69,7 +69,7 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-    SUtil.doTheCheck();
+    SUtil.check();
 	
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
