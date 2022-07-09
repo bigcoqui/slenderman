15768bc3d58e95ca23882087988ddae4d5f3dae2
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-07-09 20:52:53 -0300

    vai que da né

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -63,7 +63,7 @@ class MainMenuState extends MusicBeatState
         }
 
         #if MODS_ALLOWED
-        for (i in FileSystem.readDirectory("assets/images/menushit")) {
+        for (i in FileSystem.readDirectory(SUtil.getPath() + "assets/images/menushit")) {
             if (i.startsWith("page") && i.endsWith(".png")) {
                 var num = Std.parseInt(i.substring(4, i.length - 4).trim());
                 if (num != null && num > quant)
