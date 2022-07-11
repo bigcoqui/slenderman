38284876f8ce1dd48662fd9dbe5b9ae803c4e7b1
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-07-11 10:41:27 -0300

    update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -78,7 +78,7 @@ class TitleState extends MusicBeatState
 		Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
 
-		Paths.pushGlobalMods();
+		//Paths.pushGlobalMods(); entao n precisa
 		// Just to load a mod on start up if ya got one. For mods that change the menu music and bg
 		WeekData.loadTheFirstEnabledMod();
 
